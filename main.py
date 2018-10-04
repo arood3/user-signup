037f70a04c275d25f37261b828a50204e0399cbc
Alex Rood <alexrood@Alexs-MacBook-Pro.local>
2018-10-03 20:37:09 -0400

    Updated Errors

--- main.py
+++ main.py
@@ -24,29 +24,29 @@ def signup():
     email_error = ""
 
     if username == "": # Validate Username
-        username_error = "Please enter a valid username."
+        username_error = "Error: Please enter a valid username."
     elif len(username) <= 3 or len(username) > 20:
-        username_error = "Username must be between 3 and 20 characters long."
+        username_error = "Error: Username must be between 3 and 20 characters long."
         username = ""
     elif " " in username:
-        username_error = "Your username cannot contain any spaces."
+        username_error = "Error: Your username cannot contain any spaces."
         username = ""
 
     if password == "": # Validate Password
-        password_error = "Please enter a valid password."
+        password_error = "Error: Please enter a valid password."
     elif len(password) < 3 or len(password) > 20:
-        password_error = "Password must be between 3 and 20 characters long."
+        password_error = "Error: Password must be between 3 and 20 characters long."
     elif " " in password:
-        password_error = "Your password cannot contain any spaces."
+        password_error = "Error: Your password cannot contain any spaces."
 
     if verify == "" or verify != password: # Verify Password
-        verify_error = "Passwords do not match. Please try again."
+        verify_error = "Error: Passwords do not match. Please enter again."
         verify = ""
 
     if email != "": # Validate Email
         # Used regex for complete email validation.
         if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-                email_error = "Not a valid email address."
+                email_error = "Error: You must enter a valid email address."
 
     if not username_error and not password_error and not verify_error and not email_error:
         return render_template('welcome.html', username = username)
